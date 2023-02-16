c36f0d0956d276f6420c626a6243142fdd7861f9
Nuri Akman <nuriakman@gmail.com>
2023-02-16 14:50:05 +0300

    Update list.php

--- list.php
+++ list.php
@@ -6,4 +6,4 @@ $stmt = $conn->prepare("SELECT id, name, email FROM users");
 $stmt->execute();
 $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-echo json_encode($users);
+echo json_encode($users, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
